d964b3e611a9590d944c908a527b96ac1b82e2a6
flaming-cl <flaming-cl@hotmail.com>
2022-11-30 22:56:09 -0500

    Updates

--- example.f69400ca.js
+++ example.f69400ca.js
@@ -38816,7 +38816,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57518" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57642" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
